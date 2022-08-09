5babe4b53d4cfa1113d21720f9011dd28977ccb1
dsubnet0 <dsubnet0@gmail.com>
2022-08-08 21:26:44 -0400

    All done

--- README.md
+++ README.md
@@ -5,7 +5,24 @@ Pulls data from https://github.com/unitedstates/congress-legislators and http://
 
 ## Build and Execution
 ```bash
-pipenv install
-pipenv run python src/data_wrangler.py
+$ pipenv install
+$ pipenv run python src/data_wrangler.py
+Getting state capitals...
+parsing...
+Getting historical legislators...
+parsing...
+Getting current legislators...
+parsing...
+Iterating over all 12593 past and present legislators...
+MATCHES FOUND!
+JosiahTattnall was elected to to the great state of GA (capital Atlanta) on 1796-01-01
+EdwardTattnall was elected to to the great state of GA (capital Atlanta) on 1821-12-03
+JamesRichmond was elected to to the great state of VA (capital Richmond) on 1879-03-18
+RichardCheney was elected to to the great state of WY (capital Cheyenne) on 1979-01-15
+LizCheney was elected to to the great state of WY (capital Cheyenne) on 2017-01-03
 ```
 
+## TODO/Needed
+- State capitals change over time! What was the capital of the legislator's home state AT THE TIME THEY WERE ELECTED!
+- Unit tests
+- Refactor some of the plumbing into separate modules
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import requests 
 from typing import List, Dict
 import yaml
@@ -16,12 +17,12 @@ def _get_current_legislators() -> List:
     return yaml.safe_load(response.text)
 
 def get_all_legislators() -> List:
-    return _get_current_legislators() + _get_historical_legislators()
+    return _get_historical_legislators() + _get_current_legislators() 
 
 def _get_all_capitals() -> List:
     print(f'Getting state capitals...')
     response = requests.get('http://goodcsv.com/wp-content/uploads/2020/08/us-states-territories.csv')
-    print('parsing csv')
+    print('parsing...')
     decoded_response = response.text
     csv_reader = csv.reader(decoded_response.splitlines(), delimiter=',')
     return list(csv_reader)
@@ -63,13 +64,33 @@ def get_term_states(legislator: Dict) -> List:
 STATE_CAPITALS = _get_all_capitals() 
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='store_true')
+    args = parser.parse_args()
+
     all_legislators = get_all_legislators()
     print(f'Iterating over all {len(all_legislators)} past and present legislators...')
+    matches = []
     for legislator in all_legislators:
         name = legislator['name']['last']
-        print(f'Checking {name}...')
+        if args.verbose: print(f'Checking {name}...') 
         state_list = get_term_states(legislator)
         for state in state_list:
-            print(f'Checking against capital of {state}...')
+            if args.verbose: print(f'Checking against capital of {state}...')
             if is_letterbanked_with_capital(name, state):
-                print('MATCH!\n\n')
+                matches.append({
+                    'name': legislator['name']['first']+legislator['name']['last'],
+                    'term': legislator['terms'][0]['start'],
+                    'state': state,
+                    'capital': get_capital_from_abbrev(state)
+                })
+                #print(f'MATCH! {legislator} served the great state of {state}')
+    
+    if len(matches) > 0:
+        print(f'MATCHES FOUND!')
+        for match in matches:
+            full_name = match['name']
+            first_state = match['state']
+            capital = match['capital']
+            date_elected = match['term']
+            print(f'{full_name} was elected to to the great state of {first_state} (capital {capital}) on {date_elected}')
